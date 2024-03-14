37a4f8b13a0a4ddf4b13c11fa5d16e9e8d3c85ff
NRPM <nirupampatil44@gmail.com>
2024-03-14 16:00:20 +0530

    created resident page and other minor changes which prevent app from crashing

--- app/resident/page.js
+++ app/resident/page.js
@@ -1,9 +1,20 @@
 "use client";
 
+import SearchBar from "@/lib/components/SearchBar";
+import ResidentsTable from "@/lib/components/table/ResidentsTable";
+import { Input } from "@mui/joy";
 import React from "react";
 
 function Page() {
-  return <div>Residents</div>;
+  return (
+    <>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <SearchBar />
+
+        <ResidentsTable />
+      </div>
+    </>
+  );
 }
 
 export default Page;
